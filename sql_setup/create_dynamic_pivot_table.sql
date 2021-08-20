b6406969b4b2a07520e05ed8fc9dfbfadb394831
Kelsie Merchant <eislek02@gmail.com>
2021-08-20 23:21:22 +0000

    Add SQL script that dynamically creates pivot table based on user IDs; currently limited to even IDs to prevent exceeding limit for MySQL columns in pivot table

---
+++ sql_setup/create_dynamic_pivot_table.sql
@@ -0,0 +1,22 @@
+-- This script attempts to create a dynamic pivot table in bookBook_main_db
+-- pivots book to rows and users to columns with ratings at intersection
+
+SET group_concat_max_len = 9999999999999;
+SET @columns = NULL;
+SELECT
+	GROUP_CONCAT(DISTINCT
+		CONCAT(
+			'sum(CASE WHEN ID = ''',
+			ID,
+			''' THEN Rating END) AS ''',
+			ID,
+			''''
+		)
+	) INTO @columns
+FROM BookRatings
+WHERE ISBN < 55000000 && (ID % 2) = 0;
+SET @columns = CONCAT('SELECT ISBN, ', @columns, ' FROM BookRatings GROUP BY ISBN');
+SELECT @columns;
+PREPARE stmt FROM @columns;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
