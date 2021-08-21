57fee54c65ba6f99f093a98e692bc13079773d24
Viet <vietnamtran@gmail.com>
2021-08-20 19:21:39 -0500

    Add files for flask app

---
+++ bookbook/venv/index.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from json import loads
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Home Page Route'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api/<int:id>')
+def api(id):
+  try:
+    with open('data.json', mode='r') as my_file:
+      text = my_file.read()
+
+      for t in loads(text):
+        if t.get('id') == id:
+          return f"Hello {t.get('first_name')}"
+  except Exception:
+      return Exception
