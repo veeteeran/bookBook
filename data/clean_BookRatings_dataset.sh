cd3ad0a3c84c05b4c2024d4f248ff144c504d147
Kelsie Merchant <eislek02@gmail.com>
2021-08-27 01:21:58 -0500

    Rename script to clean BookRatings dataset

--- data/clean_BookRatings_dataset.sh
+++ data/clean_BookRatings_dataset.sh
@@ -2,7 +2,7 @@
 # cleans BookRatings dataset to only valid ISBN numbers
 # for best results, replace all 0 ratings with non-digit character before running script
 
-head -n 25 BX-Book-Ratings.sql > BookRatings.sql
+head -n 24 BX-Book-Ratings.sql > BookRatings.sql
 grep -E ",'[[:digit:]]{9}X',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
 grep -E ",'[[:digit:]]{10}',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
 grep -E ",'978[[:digit:]]{10}',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
