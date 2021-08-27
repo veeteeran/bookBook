a78a168ae94d6af09263112ad30932840c2d47bd
Kelsie Merchant <eislek02@gmail.com>
2021-08-27 02:19:07 -0500

    Add condition to remove Books.sql file if it already exists

--- data/clean_Books_dataset.sh
+++ data/clean_Books_dataset.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # cleans Books dataset to only valid ISBN numbers
 
+FILE=Books.sql
+if [[ -f "$FILE" ]]; then
+    rm Books.sql
+fi
 head -n 24 BX-Books.sql > Books.sql
 grep -E "VALUES \('[[:digit:]]{9}X'," BX-Books.sql >> Books.sql
 grep -E "VALUES \('[[:digit:]]{9}x'," BX-Books.sql >> Books.sql
