bb636c475eb2942226c0a226a692e88b05204d63
Kelsie Merchant <eislek02@gmail.com>
2021-08-26 19:52:47 -0500

    Add Bash script that cleans the BX-Book-Ratings.sql dataset and saves it to BookRatings.sql file

---
+++ data/clean_dataset.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# cleans BookRatings dataset to only valid ISBN numbers
+# for best results, replace all 0 ratings with non-digit character before running script
+
+head -n 25 BX-Book-Ratings.sql > BookRatings.sql
+grep -E ",'[[:digit:]]{9}X',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
+grep -E ",'[[:digit:]]{10}',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
+grep -E ",'978[[:digit:]]{10}',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
+grep -E ",'979[[:digit:]]{10}',[[:digit:]]*);" BX-Book-Ratings.sql >> BookRatings.sql
