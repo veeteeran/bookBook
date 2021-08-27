aaca0f468f4b23a9e692563181d803643600eaa9
Kelsie Merchant <eislek02@gmail.com>
2021-08-27 03:08:13 -0500

    Add test files to check output of dynamic pivot table with smaller dataset

---
+++ test/create_dynamic_pivot_table.sql
@@ -0,0 +1,22 @@
+-- This script attempts to create a dynamic pivot table in bookBook_main_db
+-- pivots book to rows and users to columns with ratings at intersection
+
+SET group_concat_max_len = 9999999999999;
+SET @columns = NULL;
+SELECT
+	GROUP_CONCAT(DISTINCT
+		CONCAT(
+			'sum(CASE WHEN ID = ''',
+			ID,
+			''' THEN Rating END) AS ''',
+			ID,
+			''''
+		)
+	) INTO @columns
+FROM MovieRatings
+DROP TABLE IF EXISTS Pivot;
+SET @columns = CONCAT('CREATE TABLE Pivot SELECT Title, ', @columns, ' FROM MovieRatings GROUP BY Title');
+SELECT @columns;
+PREPARE stmt FROM @columns;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
