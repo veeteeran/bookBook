becfc97ce026f0aeda21faa49386533fd61c3bff
Kelsie Merchant <eislek02@gmail.com>
2021-08-27 21:30:14 +0000

    Update column names to match case of SQL table and remove unnecessary import

--- models/movie_ratings.py
+++ models/movie_ratings.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ this module builds MovieRatings class """
 from base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer
 
 class MovieRatings(BaseModel, Base):
   """ this module defines BaseRating inherited by all classes
   """
   __tablename__ = 'MovieRatings'
-  id = Column(Integer, primary_key=True)
-  rating = Column(Integer, nullable=False, default=0)
+  ID = Column(Integer, primary_key=True)
+  Rating = Column(Integer, nullable=False, default=0)
