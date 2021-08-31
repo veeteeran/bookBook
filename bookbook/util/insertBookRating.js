eae31040b4df4fe06ba60ab8fcdbbfad0d00bd0a
Viet <vietnamtran@gmail.com>
2021-08-30 20:57:35 -0500

    Rename directory

--- bookbook/util/insertBookRating.js
+++ bookbook/util/insertBookRating.js
@@ -18,16 +18,19 @@ const mysql = require('serverless-mysql')({
 })
 
 // Main handler function
-const insertBookRating = async (arg) => {
+const insertBookRating = async (title = '', rating = 0, id = null) => {
   // Run your query
-  const results = await mysql.query('SELECT ISBN FROM Books WHERE Title=?', [arg[0]])
-
+  const results = await mysql.query('SELECT ISBN FROM Books WHERE Title=?', title)
+  let newId = 0
   if (results.length > 0) {
-    const lastId = await mysql.query('SELECT ID FROM BookRatings order by id desc limit 1')
+    if (!id) {
+      const lastId = await mysql.query('SELECT ID FROM BookRatings order by id desc limit 1')
+      newId = (parseInt(lastId[0].ID) + 1)
+    }
 
     await mysql.query(
       'INSERT INTO BookRatings(ID, ISBN, Rating) VALUES(?, ?, ?)',
-      [(parseInt(lastId[0].ID) + 1), results[0].ISBN, arg[1]])
+      [newId, results[0].ISBN, rating])
       .catch(err => console.log(err))
   }
 
@@ -41,6 +44,11 @@ const insertBookRating = async (arg) => {
   // Give clarification if length is > 0
   // Trigger bookEater if no results found?
   // To be done outside of this function?
+  return { results, newId }
 }
 
-export default insertBookRating
+// export default insertBookRating
+
+const { results, newId } = insertBookRating('Dune', 10)
+
+console.log(results, newId)
