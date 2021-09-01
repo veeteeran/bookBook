5b69e023cec4122c57026c0fe731e8e3619c64ef
Kelsie Merchant <eislek02@gmail.com>
2021-09-01 02:50:54 +0000

    Fix import error to include module name and add Title column to identify movie Title

--- models/movie_ratings.py
+++ models/movie_ratings.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ this module builds MovieRatings class """
-from base_model import BaseModel, Base
-from sqlalchemy import Column, Integer
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 class MovieRatings(BaseModel, Base):
   """ this module defines BaseRating inherited by all classes
   """
   __tablename__ = 'MovieRatings'
+  Title = Column(String(13),
+                 nullable=False,
+                 primary_key=True)
   ID = Column(Integer, primary_key=True)
   Rating = Column(Integer, nullable=False, default=0)
