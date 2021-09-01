a57d592626cb85112b883ae284010fd05f22a5f9
Kelsie Merchant <eislek02@gmail.com>
2021-09-01 02:50:25 +0000

    Fix import error to include module name and add ISBN column to identify book ISBN

--- models/book_ratings.py
+++ models/book_ratings.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ this module builds BookRatings class """
-from base_model import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer
 
 class BookRatings(BaseModel, Base):
   """ this module defines BaseRating inherited by all classes
   """
   __tablename__ = 'BookRatings'
+  ISBN = Column(String(13),
+                nullable=False,
+                primary_key=True)
   ID = Column(Integer, primary_key=True)
   Rating = Column(Integer, nullable=False, default=0)
