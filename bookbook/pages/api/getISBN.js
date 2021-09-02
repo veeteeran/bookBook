54528db960e1a74fba13b37b18ec7748fc18a44c
Viet <vietnamtran@gmail.com>
2021-09-02 13:31:01 -0500

    Rename file, fix linting issues

--- bookbook/pages/api/getISBN.js
+++ bookbook/pages/api/getISBN.js
@@ -2,13 +2,10 @@ import executeQuery from '../../util/db'
 
 export default async (req, res) => {
   try {
-    console.log("req nom", req.query)
     const results = await executeQuery({
       query: 'SELECT ISBN FROM Books WHERE Title=?',
       values: req.query.title,
     })
-    console.log('ISBN', results[0].ISBN)
-    console.log('userId from req object', req.query.userId)
     let newId = 0
     if (results && results.length > 0) {
       if (!req.query.userId) {
@@ -16,14 +13,11 @@ export default async (req, res) => {
           query: 'SELECT ID FROM BookRatings order by id desc limit 1',
           values: []
         })
-        console.log('lastId', lastId)
         newId = (parseInt(lastId[0].ID) + 1)
       } else {
         newId = req.query.userId
       }
     }
-
-    console.log(newId)
 
     await executeQuery(
       {
--- bookbook/src/components/Welcome/Welcome.tsx
+++ bookbook/src/components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const InputTest = () => {
   const [textInput, setTextInput] = useState('')
