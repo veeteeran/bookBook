56e093ab103fd3c887898d259f6c99bc251879fc
Viet <vietnamtran@gmail.com>
2021-09-03 17:07:24 -0500

    Add header component

--- bookbook/src/components/Header/Header.tsx
+++ bookbook/src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+const styles = require('./header.module.scss')
 
 const Header = () => {
   return (
-    <div>
-      {/*  */}
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.icon} />
+        <div className={styles.title}>bookBook</div>
+        <div className={styles.getList} />
+      </div>
     </div>
   )
 }
--- bookbook/src/components/Welcome/Welcome.tsx
+++ bookbook/src/components/Welcome/Welcome.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
+import { Box } from '@material-ui/core'
+import Header from '../Header/Header'
+import Rating from '@material-ui/lab/Rating'
 import React, { useState } from 'react'
 
 const labels = {
