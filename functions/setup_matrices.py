45e46eeef99f19e8f6265f3d9dfa07f18f2114ce
Kelsie Merchant <eislek02@gmail.com>
2021-09-08 19:26:35 +0000

    Change function to set up pivot table matrices to reference books instead of movies

--- functions/setup_matrices.py
+++ functions/setup_matrices.py
@@ -6,16 +6,16 @@ transform that pivot table into Numpy ndarrays and Pandas DataFrame
 
 
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
 
 def pivot_table(ratings_list):
     """
-    Creates a pivot table with users as rows and movies as columns
+    Creates a pivot table with users as rows and books as columns
 
     parameters:
         ratings_list [list of dicts]:
-            represents the table MovieRatings
+            represents the table BookRatings
 
     returns:
         pivot table
@@ -23,18 +23,18 @@ def pivot_table(ratings_list):
     result = {}
     # ysort is list of all user IDs
     ysort = []
-    # xsort is list of all movie Titles
+    # xsort is list of all book ISBNs
     xsort = []
-    # loop through every row in MovieRatings table representation
+    # loop through every row in BookRatings table representation
     for row in ratings_list:
         # yaxis is current row's user ID
         yaxis = row["ID"]
         # if user ID is not yet in ysort, it saves to ysort
         if yaxis not in ysort:
             ysort.append(yaxis)
-        # xaxis is current row's movie Title
-        xaxis = row["Title"]
-        # if movie Title is not yet in xsort, it saves to xsort
+        # xaxis is current row's book ISBN
+        xaxis = row["ISBN"]
+        # if book ISBN is not yet in xsort, it saves to xsort
         if xaxis not in xsort:
             xsort.append(xaxis)
         # tries user ID as key in results dictionary
@@ -43,24 +43,24 @@ def pivot_table(ratings_list):
         except KeyError:
             # if user ID is not yet a key in results dictionary, creates one
             result[yaxis] = {}
-        # tests if movie Title is in results dictionary under user ID key
+        # tests if book ISBN is in results dictionary under user ID key
         if xaxis not in result[yaxis]:
-            # if not, add 0 to create placeholder under user ID, movie Title
+            # if not, add 0 to create placeholder under user ID, book ISBN
             result[yaxis][xaxis] = 0
-        # saves Rating in results dictionary under user ID and movie Title
+        # saves Rating in results dictionary under user ID and book ISBN
         result[yaxis][xaxis] = row["Rating"]
     # loops through all user IDs
     for yaxis in ysort:
-        # loops through all movie Titles
+        # loops through all book ISBNs
         for xaxis in xsort:
-            # if movie Title does not already exist within user ID,
+            # if book ISBN does not already exist within user ID,
             #     saves as 0 rating
             if xaxis not in result[yaxis]:
                 result[yaxis][xaxis] = 0
 
     # creates list headings with ID first
     headings = ["ID"]
-    # extends headings list with all movie Titles
+    # extends headings list with all book ISBNs
     headings.extend(xsort)
 
     # creates pivot table as list of dictionaries
@@ -69,7 +69,7 @@ def pivot_table(ratings_list):
     for user_id in ysort:
         # creates row based on user ID value
         row = [user_id]
-        # extends row with user ratings for each movie Title
+        # extends row with user ratings for each book ISBN
         row.extend([result[user_id][x] for x in xsort])
         # appends dictionary with keys from headings and
         #    values from row to pivot_table
@@ -81,8 +81,8 @@ def pivot_table(ratings_list):
 def to_matrices(pivot_table, user_id):
     """
     Turns a list of dicts pivot table into matrices
-        representing specific user ratings of movies
-        and all other user ratings of movies
+        representing specific user ratings of books
+        and all other user ratings of books
 
     parameters:
         pivot_table [list of dicts]:
@@ -91,12 +91,14 @@ def to_matrices(pivot_table, user_id):
             value of user id finding recommendations for
 
     returns:
-        data [Pandas DataFrame]:
-            DataFrame representation of the pivot table,
-                excluding user finding recommendation for
-        user [Pandas DataFrame]:
-            DataFrame representation of the pivot table,
+        matrix [list of lists]:
+            matrix representation of pivot table,
+                all users except specific user
+        user_row [list of lists]:
+            matrix representation of the pivot table,
                 specifically for user's ratings
+        headings [list]:
+            list of just headings with all book ISBNs
     """
     # matrix will be list of lists of all user ratings
     #     excluding the user finding recommendation for
@@ -106,9 +108,9 @@ def to_matrices(pivot_table, user_id):
     # headings is all column names
     headings = ["ID"]
     # adds the keys from dicts as headings for matrix
-    for title in pivot_table[0].keys():
-        if title != "ID":
-            headings.append(title)
+    for ISBN in pivot_table[0].keys():
+        if ISBN != "ID":
+            headings.append(ISBN)
     # adds headings as first row of matrix and user
     matrix.append(headings)
     user_row.append(headings)
@@ -131,8 +133,8 @@ def to_matrices(pivot_table, user_id):
 def to_numpy(pivot_table, user_id):
     """
     Turns a list of dicts pivot table into numpy.ndarrays
-        representing specific user ratings of movies
-        and all other user ratings of movies
+        representing specific user ratings of books
+        and all other user ratings of books
 
     parameters:
         pivot_table [list of dicts]:
@@ -143,7 +145,7 @@ def to_numpy(pivot_table, user_id):
     returns:
         data [numpy.ndarray]:
             numpy array representation of the pivot table,
-                excluding user finding recommendation for
+                all users except user finding recommendation for
         user [numpy.ndarray]:
             numpy array representation of the pivot table,
                 specifically for user's ratings
@@ -159,8 +161,8 @@ def to_numpy(pivot_table, user_id):
 def to_pandas(pivot_table, user_id):
     """
     Turns a list of dicts pivot table into Pandas DataFrames
-        representing specific user ratings of movies
-        and all other user ratings of movies
+        representing specific user ratings of books
+        and all other user ratings of books
 
     parameters:
         pivot_table [list of dicts]:
@@ -171,7 +173,7 @@ def to_pandas(pivot_table, user_id):
     returns:
         data [pd.DataFrame]:
             Pandas DataFrame representation of the pivot table,
-                excluding user finding recommendation for
+                all users except user finding recommendation for
         user [pd.DataFrame]:
             Pandas DataFrame representation of the pivot table,
                 specifically for user's ratings
@@ -182,10 +184,10 @@ def to_pandas(pivot_table, user_id):
     data_array = np.array(matrix, ndmin=2)
     user_array = np.array(user_row, ndmin=2)
     # turns numpy.ndarray into Pandas DataFrames
-    data = pd.DataFrame(data_array[1:])
-    user = pd.DataFrame(user_array[1:])
+    # data = pd.DataFrame(data_array[1:])
+    # user = pd.DataFrame(user_array[1:])
     # adds headers as column names to DataFrame
-    data.columns = headings
-    user.columns = headings
+    # data.columns = headings
+    # user.columns = headings
     # returns Pandas DataFrame representation of ratings matrix
-    return data, user
+    return data_array, user_array
