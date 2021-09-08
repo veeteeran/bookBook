1b768c127f05b8d3903f9f413375a86f439e97c8
Viet <vietnamtran@gmail.com>
2021-09-08 16:18:32 -0500

    Add get_list route to test the model

--- bookbook/venv/index.py
+++ bookbook/venv/index.py
@@ -42,3 +42,34 @@ def api(id):
       return f'ID: {id} not found'
   except Exception:
       return Exception
+
+@app.route('/get_list/<int:id>')
+def get_list(id):
+    from functions.setup_matrices import pivot_table
+    from functions.setup_matrices import to_numpy
+    from functions.similarity import similar_ratings
+    from functions.predictions import predict_ratings
+    from functions.parse import get_recommendations
+    from models import storage
+
+    ratings_dict, ratings_list = storage.all("BookRatings")
+    print("got BookRatings from storage")
+
+    pt = pivot_table(ratings_list)
+    print("got pivot table")
+    data, user = to_numpy(pt, id)
+
+    print("got numpy.ndarrays")
+    data_num = data[1:].astype(int)
+    user_num = user[1:].astype(int)
+    indices, cos_sim = similar_ratings(data_num, user_num)
+    print("got cosine similarities")
+    user_predictions, predicted_indices = predict_ratings(
+        user, data_num, user_num, indices, cos_sim)
+
+    print("got predicted ratings")
+    sorted_ISBNs = get_recommendations(user_predictions, predicted_indices)
+    
+    print(sorted_ISBNs)
+
+    return sorted_ISBNs
