c3188a11a4bf95a390845b65d13634feb8cd0f83
Viet <vietnamtran@gmail.com>
2021-09-09 16:05:03 -0500

    Add carousel component

--- bookbook/src/components/BookCarousel/BookCarousel.tsx
+++ bookbook/src/components/BookCarousel/BookCarousel.tsx
@@ -1,92 +1,85 @@
 import React, { useEffect, useState } from 'react'
 const styles = require('./carousel.module.scss')
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
+import { useSpringCarousel } from 'react-spring-carousel-js'
 
 const BookCarousel = () => {
+  const [listExists, setListExists] = useState(false)
   const [booksData, setBooksData] = useState([])
+  const [booksArray, setBooksArray] = useState([])
 
+  // const fetchData = async () => {
+  //   await fetch(`/api/getBookData`)
+  //     .then(response => response.json())
+  //     .then(data => setBooksData(data))
+  //     .catch(err => console.log(err))
+  // }
+
+  // console.log(`BOOKSDATA:`, booksData)
+
+  const createCarouselArray = (data) => {
+    return data.map((book, i) => (
+      {
+        id: `item-${i + 1}`,
+        renderItem:
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }} >
+            <div style={{
+              backgroundImage: `url(${book.ImageLarge})`,
+              width: '10vw',
+              height: '10vw',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center'
+            }} />
+            <p>{book.Title}</p>
+            <p>{book.Author}</p>
+          </div>
+      }
+    ))
+  }
+
+  const doIt = () => {
+    // fetchData()
+    const foo = createCarouselArray(booksData)
+    setBooksArray(foo)
+    setListExists(true)
+  }
+  console.log(`bOOKSaRRAY: `, booksArray)
   useEffect(() => {
+    let cancel = false
     const fetchData = async () => {
       await fetch(`/api/getBookData`)
         .then(response => response.json())
-        .then(data => { setBooksData(data) })
+        .then(data => setBooksData(data))
         .catch(err => console.log(err))
     }
     fetchData()
+    return () => {
+      cancel = true
+    }
   }, [])
+
+  const { carouselFragment } = useSpringCarousel({
+    withThumbs: false,
+    withLoop: true,
+    itemsPerSlide: listExists ? 5 : 1,
+    initialStartingPosition: 'center',
+    items: listExists ? booksArray : [{ id: "item-1", renderItem: <p>List does not exist</p> }]
+  });
   return (
     <div className={styles.section}>
-      {booksData.map((book, i) => (
-        <div key={i} className={styles.container}>
-          <Carousel responsive={responsive}>
-            {/* <div className={styles.image} style={{ backgroundImage: `url(${book.ImageLarge})` }} />
-            <p>{book.Title}</p>
-            <p>{book.Author}</p> */}
-            <img
-              src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-              style={{
-                display: 'block',
-                height: '100%',
-                margin: 'auto',
-                width: '100%'
-              }}
-            />
-            <img
-              src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-              style={{
-                display: 'block',
-                height: '100%',
-                margin: 'auto',
-                width: '100%'
-              }}
-            />
-          </Carousel>
-        </div>
-      ))}
-      {/* <Carousel responsive={responsive}>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel> */}
-      <img
-        src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        style={{
-          display: 'block',
-          height: '100%',
-          margin: 'auto',
-          width: '100%'
-        }}
-      />
-      <img
-        src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-        style={{
-          display: 'block',
-          height: '100%',
-          margin: 'auto',
-          width: '100%'
-        }}
-      />
+      {
+        listExists
+          ? <div style={{
+            width: '100%', display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', overflow: 'hidden'
+          }}>
+            {carouselFragment}
+          </div>
+          : <button onClick={doIt}>Get List</button>
+      }
     </div>
   )
 }
