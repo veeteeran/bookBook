05cfe92cbb309a660c15f070f8bbc1763e33d8fe
Kelsie Merchant <eislek02@gmail.com>
2021-09-11 16:32:23 +0000

    Update sorting to ignore 0 ratings and ensure 10 ratings are sorted as the best rating, not sorted based on first digit

--- functions/parse.py
+++ functions/parse.py
@@ -26,9 +26,16 @@ def get_recommendations(user_predictions, predicted_indices, N=10):
     predicted_ratings = []
     # loop through each predicted index
     for index in predicted_indices:
+        rating = user_predictions[1][index]
+        if rating == "0":
+            # if rating is 0, ignore and continue on
+            continue
+        if rating != "10":
+            # if rating is less than 10, prepend a 0 for sorting
+            rating = "0" + rating
         # dictionary stores both ISBN and Rating associated with index
         dictionary = {"ISBN":user_predictions[0][index],
-                      "Rating":user_predictions[1][index]}
+                      "Rating":rating}
         # append dictionary to predicted_ratings list
         predicted_ratings.append(dictionary)
     # sorts predicted_ratings by Rating value
@@ -40,7 +47,7 @@ def get_recommendations(user_predictions, predicted_indices, N=10):
     if len(predicted_ratings) > N:
         sorted_predicted_ratings = sorted_predicted_ratings[:N]
     # prints sorted predicted ratings for easy viewing
-    print(sorted_predicted_ratings)
+    # print(sorted_predicted_ratings)
     # sorted_ISBNs will be sorted list of only book ISBNs
     sorted_ISBNs = []
     # loop through sorted predicted ratings to get ISBN value
