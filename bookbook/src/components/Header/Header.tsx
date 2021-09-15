842c8bf39d8993e96e618e18303e9a10c911e88a
Viet <vietnamtran@gmail.com>
2021-09-15 16:42:15 -0500

    Add button back to main page

--- bookbook/src/components/Header/Header.tsx
+++ bookbook/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 const styles = require('./header.module.scss')
 
 const Header = () => {
   return (
     <div className={styles.section}>
       <div className={styles.container}>
-        <div className={styles.logo}></div>
+        <Button className={styles.logo} href="/" />
         <div className={styles.bB} />
       </div>
     </div>
