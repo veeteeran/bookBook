6b774d5b0f4f034fe81724c648a629b71a4d7626
Kelsie Merchant <eislek02@gmail.com>
2021-09-15 03:20:21 +0000

    Remove print statements used for testing timing during program runtime and add N parameter to find 2 most similar users to calculate ratings from

--- book_recommender.py
+++ book_recommender.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     # ratings_dict is dictionary of objects
     # ratings_list is list of dictionary representation
     ratings_dict, ratings_list = storage.all("BookRatings")
-    print("got BookRatings from storage")
     # creates pivot table from ratings_list
     #    with user IDs as rows, ISBNs as columns
     #    and ratings where user ID and book ISBN meet
@@ -30,25 +29,21 @@ if __name__ == "__main__":
     # for row in pt:
     # print(row)
     # turns pivot table into numpy.ndarrays
-    print("got pivot table")
     data, user = to_numpy(pt, int(argv[1]))
     # prints the np.ndarrays for easy viewing
     # print(data)
     # print(user)
     # data_num and user_num are the numbers-only numpy.ndarrays
     # first headings row is removed and type changed to ints
-    print("got numpy.ndarrays")
     data_num = data[1:].astype(int)
     user_num = user[1:].astype(int)
-    indices, cos_sim = similar_ratings(data_num, user_num)
-    print("got cosine similarities")
+    indices, cos_sim = similar_ratings(data_num, user_num, 2)
     user_predictions, predicted_indices = predict_ratings(
         user, data_num, user_num, indices, cos_sim)
     # prints the updated user for easy viewing
     # print(user_predictions)
     # prints the list of indices where book rating was predicted
     # print(predicted_indices)
-    print("got predicted ratings")
     sorted_ISBNs = get_recommendations(user_predictions, predicted_indices)
     # prints the sorted ISBN recommendations for easy viewing
     print(sorted_ISBNs)
